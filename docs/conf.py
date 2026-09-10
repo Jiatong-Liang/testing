6045e3e59903408207a0e9d858db33f00a17a605
Jiatong <jkliang@umich.edu>
2026-09-09 20:23:57 -0400

    update

--- docs/conf.py
+++ docs/conf.py
@@ -91,8 +91,8 @@ source_suffix = [".rst", ".md"]
 master_doc = "index"
 
 # General information about the project.
-project = "momi3"
-copyright = "2023, Enes Dilber and Jonathan Terhorst"
+project = "dreems"
+copyright = "2026, Jiatong Liang and Jonathan Terhorst"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -103,7 +103,7 @@ copyright = "2023, Enes Dilber and Jonathan Terhorst"
 # If you don’t need the separation provided between version and release,
 # just set them both to the same value.
 try:
-    from demes import __version__ as version
+    from dreems import __version__ as version
 except ImportError:
     version = ""
 
@@ -235,7 +235,7 @@ html_static_path = ["_static"]
 # html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = "momi3-doc"
+htmlhelp_basename = "dreems-doc"
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -252,7 +252,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-    ("index", "user_guide.tex", "momi3 Documentation", "Enes Dilber and Jonathan Terhorst", "manual")
+    ("index", "user_guide.tex", "dreems Documentation", "Jiatong Liang and Jonathan Terhorst", "manual")
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
